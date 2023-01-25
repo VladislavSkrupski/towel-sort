763f9adfd04495bcb4ecf754f62b158d1a686082
Vladislav Skrupski <vladislav.skrupski@gmail.com>
2023-01-25 12:35:47 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+    if (matrix.length === 0) return [];
+    let linearArray = [];
+    matrix.forEach((subArray, index) => {
+        linearArray = linearArray.concat(index % 2 === 0 ? subArray : subArray.reverse());
+    })
+    return linearArray;
 }
